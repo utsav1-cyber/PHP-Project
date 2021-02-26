e1674659bc273bbd77204e3843ba6d384ab8992d
Utsav Thakur <77831507+utsav1-cyber@users.noreply.github.com>
2021-02-26 18:47:11 +0545

    Add files via upload

---
+++ php.dev/config.php
@@ -0,0 +1,11 @@
+<?php
+
+// Define DB Params
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASS", "");
+define("DB_NAME", "myblog");
+
+// Define URL
+define("ROOT_PATH", "/php.dev/");
+define("ROOT_URL", "http://localhost/php.dev/");
