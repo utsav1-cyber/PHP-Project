b5c8f0ac26abd44895809c7d3231a7c486d47aa5
Utsav Thakur <77831507+utsav1-cyber@users.noreply.github.com>
2021-02-28 12:32:44 +0545

    Update index.php

--- php.dev/index.php
+++ php.dev/index.php
@@ -1,7 +1,11 @@
 <?php
+// Start Session
+session_start();
+
 // Include Config
 require('config.php');
 
+require('classes/Messages.php');
 require('classes/Bootstrap.php');
 require('classes/Controller.php');
 require('classes/Model.php');
